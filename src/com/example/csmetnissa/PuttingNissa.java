d726d9f55b590e65740207e41a1e94ee6e18442f
Nayar Joolfoo <njoolfoo@gmail.com>
2013-07-03 23:40:26 +0400

    Said stop putting more

--- src/com/example/csmetnissa/PuttingNissa.java
+++ src/com/example/csmetnissa/PuttingNissa.java
@@ -3,14 +3,25 @@ package com.example.csmetnissa;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class PuttingNissa extends Activity {
 
 	protected void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
 	        setContentView(R.layout.putting_nissa);
-	        Button BPutMoreNissa = (Button) (Button) findViewById(R.id.button2);
+	        Button BPutMoreNissa = (Button) findViewById(R.id.button2);
+	        final TextView sakili = (TextView) findViewById(R.id.textView2);
+	        BPutMoreNissa.setOnClickListener(new View.OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					// TODO Auto-generated method stub
+					sakili.setText("Assez matelot!!!");
+				}
+			});
 	    }
 	
 	    @Override
