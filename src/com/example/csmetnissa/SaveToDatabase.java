02f981916f68156d14c817d919f0528a8338ae83
Nayar Joolfoo <njoolfoo@gmail.com>
2013-07-05 15:13:20 +0400

    added seekbar listener

--- src/com/example/csmetnissa/SaveToDatabase.java
+++ src/com/example/csmetnissa/SaveToDatabase.java
@@ -3,15 +3,46 @@ package com.example.csmetnissa;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
 
 public class SaveToDatabase extends Activity {
-
+	int amt = 0;
+	TextView tv;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_save_to_database);
+		
+		SeekBar sb = (SeekBar) findViewById(R.id.seekBar1);		
+		sb.setOnSeekBarChangeListener(new MeraSeekBarListener());
+		
+		tv = (TextView) findViewById(R.id.textView2);
+		
 	}
-
+	
+	class MeraSeekBarListener implements OnSeekBarChangeListener{
+		
+		@Override
+		public void onStopTrackingTouch(SeekBar seekBar) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+		@Override
+		public void onStartTrackingTouch(SeekBar seekBar) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+		@Override
+		public void onProgressChanged(SeekBar seekBar, int progress,
+				boolean fromUser) {
+			// TODO Auto-generated method stub
+			tv.setText("Amount of nissa put: "+Integer.toString(progress));
+		}
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
