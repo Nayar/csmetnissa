4e7305ec81ef71e4e15b089eecf68b0060af632e
Nayar Joolfoo <njoolfoo@gmail.com>
2013-07-06 11:42:07 +0400

    Added GPS location activity

---
+++ src/com/example/csmetnissa/GetGPSLocation.java
@@ -0,0 +1,22 @@
+package com.example.csmetnissa;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class GetGPSLocation extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_get_gpslocation);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.get_gpslocation, menu);
+		return true;
+	}
+
+}
